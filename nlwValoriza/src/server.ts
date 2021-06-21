e9c1e13461134830378763df0c7380a85b06059d
a <a@b.b>
2021-06-21 10:21:10 -0300

    POST

---
+++ nlwValoriza/src/server.ts
@@ -0,0 +1,36 @@
+import express, { request, response } from "express";
+
+
+
+const app = express();
+
+
+/**
+ * GET => Buscar uma informação 
+ * POST => Inserir (Criar) uma informação
+ * PUT =: Alterar uma informaçã
+ * DELETE => Remover um dado
+ * PATCH => Alterar uma informação especifica
+ */
+
+
+app.get("/test", (request, response) => {
+    // Request => Entrando
+    // Response => Saindo
+
+
+    return response.send('<h1> Olá </h1>');
+
+})
+
+
+
+app.post("/test-post", (request,response) => {
+
+    return response.send("POST 1");
+
+})
+
+
+
+app.listen(3000, () => console.log("Inicializado!!! https://localhost:3000")); 
