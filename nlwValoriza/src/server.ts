0c3a9719b0b3e69a153d009641d1e74890abed92
a <a@b.b>
2021-06-23 17:44:25 -0300

    db

--- nlwValoriza/src/server.ts
+++ nlwValoriza/src/server.ts
@@ -1,52 +1,10 @@
-import express, { request, response } from "express";
+import express from "express";
+import "reflect-metadata";
+import "./database"
 
 
 
 const app = express();
-
-
-/**
- * GET => Buscar uma informação 
- * POST => Inserir (Criar) uma informação
- * PUT =: Alterar uma informaçã
- * DELETE => Remover um dado
- * PATCH => Alterar uma informação especifica
- */
-
-
-/**
- * Tipos de parâmetros
- * 
- * REQUISIÇÃO GET, PUT
- * Routes Params => http://localhost:3000/produtos/3798248973284 "!ID por exemplo"
- * Query Params => http://localhost:3000/produtos?name=teclado
- * 
- * 
- * REQUISIÇÃO POST
- * BOdy Params => {
- *  "name": "teclado",
- *  "description": "teclado" 
- * }
- * 
- */
-
-app.get("/test", (request, response) => {
-    // Request => Entrando
-    // Response => Saindo
-
-
-    return response.send('<title>ola</title><h1> Olá </h1>');
-
-})
-
-
-
-app.post("/test-post", (request,response) => {
-
-    return response.send("POST 1");
-
-})
-
 
 
 app.listen(3000, () => console.log("Inicializado!!! https://localhost:3000")); 
